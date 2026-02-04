f30477c72ad91611642bb192924907357d7638ee
niteshjaitwar <nitesh.jaitwar@outlook.com>
2026-02-04 18:01:09 +0530

    fix(test): Mock external services in tests to ensure consistent CI build

--- src/test/java/com/nexra/user_service/UserServiceApplicationTests.java
+++ src/test/java/com/nexra/user_service/UserServiceApplicationTests.java
@@ -6,6 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserServiceApplicationTests {
 
+	@org.springframework.boot.test.mock.mockito.MockBean
+	private org.springframework.data.redis.core.RedisTemplate<String, Object> redisTemplate;
+
+	@org.springframework.boot.test.mock.mockito.MockBean
+	private org.springframework.kafka.core.KafkaTemplate<String, Object> kafkaTemplate;
+
+	@org.springframework.boot.test.mock.mockito.MockBean
+	private org.springframework.mail.javamail.JavaMailSender javaMailSender;
+
 	@Test
 	void contextLoads() {
 	}
